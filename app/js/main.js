3002eef8ba68fc28ec8ebf18175fac7b7de917a2
Mykhailo Khamardiuk <mishakhamardiuk@gmail.com>
2022-07-23 14:13:41 -0400

    Finished header slider

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,12 @@ $(function(){
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="prev">',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="right">',
+    asNavFor: '.slider-dots'
   });
-
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider'
+  });
   
 });
