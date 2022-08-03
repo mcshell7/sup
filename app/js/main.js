941ff88e52fe9427a565b99ea8787cff68321b30
Mykhailo Khamardiuk <mishakhamardiuk@gmail.com>
2022-08-03 11:55:02 -0400

    html for map section

--- app/js/main.js
+++ app/js/main.js
@@ -5,12 +5,18 @@ $(function(){
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="prev">',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="right">',
-    asNavFor: '.slider-dots'
+    asNavFor: '.slider-dotshead'
   });
-  $('.slider-dots').slick({
+  $('.slider-dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header__slider'
   });
   
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="prev">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="right">',
+  })
 });
